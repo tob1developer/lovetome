c63dd848bda280c8b347f6cfcd641af23c552e40
kiet <kiettn@toppy.edu.vn>
2020-03-26 14:09:24 +0700

    hoc data class

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,3 +1,26 @@
 fun main(){
-    print("doi co dep lam khong?")
+    data class person(var name:String, var age:Int, var phone : String)
+    class NguoiIu(private var name: String, private var age: Int, private var phone: String)
+    val Kiet: person = person("Ngo Tuan Kiet",20,"0336391269")
+        //get
+    println(Kiet.name)
+    println(Kiet.age)
+    println(Kiet.phone)
+    println(Kiet)
+    println(Kiet.component1())
+    println(Kiet.component2())
+    println(Kiet.component3())
+
+        //set
+    Kiet.name = "Love you du du du"
+    println(Kiet.name)
+
+        //private
+    var banNaoDo: NguoiIu = NguoiIu(":v lam gi co",20,"nope")
+    println(banNaoDo)
+
+        //copy
+    val betterKiet = Kiet.copy(name = "love you so much")
+    println(betterKiet.name)
+        //
 }

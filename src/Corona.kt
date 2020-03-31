8bd90ade9ab41d4dfeebc32d06abcb95a13d085e
kiet <kiettn@toppy.edu.vn>
2020-03-31 22:51:17 +0700

    done bai 2

---
+++ src/Corona.kt
@@ -0,0 +1,17 @@
+import com.google.gson.annotations.SerializedName
+
+//(val Country: String, val cases:Int, val todayCases:Int, val Deaths:Int,val TodayDeaths: Int, val recovered: Int)
+data class Corona(
+    @field:SerializedName("country")
+    val Country: String? =null,
+    @field:SerializedName("cases")
+    val Cases: Int? =null,
+    @field:SerializedName("todayCases")
+    val TodayCases: Int? =null,
+    @field:SerializedName("deaths")
+    val Deaths: Int? =null,
+    @field:SerializedName("todayDeaths")
+    val TodayDeaths: Int? =null,
+    @field:SerializedName("recovered")
+    val Recovered: Int? =null
+)
